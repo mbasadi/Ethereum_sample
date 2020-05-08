b5b9a1ae505af9cd0c69e10a4b6bdca75440af44
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-08 21:44:50 +0000

    Deploy mbasadi/Ethereum_sample to github.com/mbasadi/Ethereum_sample.git:gh-pages

---
+++ precache-manifest.38636a743112d8f1fefa23b9edc065ec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fab21ddb6399c34721a9cc464fa708",
+    "url": "/Ethereum_sample/index.html"
+  },
+  {
+    "revision": "359f9ac8a6b033d91620",
+    "url": "/Ethereum_sample/static/js/2.ca2dc9f7.chunk.js"
+  },
+  {
+    "revision": "deea066462e3852fb7f610c3fa67aa20",
+    "url": "/Ethereum_sample/static/js/2.ca2dc9f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57781950d4fcd44554fd",
+    "url": "/Ethereum_sample/static/js/main.26f1279f.chunk.js"
+  },
+  {
+    "revision": "5412c345b4c70ad95a2f",
+    "url": "/Ethereum_sample/static/js/runtime-main.27cfaad0.js"
+  }
+]);
